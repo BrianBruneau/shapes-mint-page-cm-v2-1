74803ab7dff00d7f4c06f32deab7cbfc6804a6d8
brian bruneau <brianbruneauz@gmail.com>
2022-04-11 22:38:08 -0500

    update mint counter to 0 sold out

--- src/Header.tsx
+++ src/Header.tsx
@@ -27,7 +27,7 @@ export const Header = ({ candyMachine }: HeaderProps) => {
                   fontWeight: 'bold',
                 }}
               >
-                {`${((candyMachine?.state.itemsRemaining) - 3661 ) }`}
+                {`${((candyMachine?.state.itemsRemaining) - 3673 ) }`}
               </Typography>
             </Grid>
             <Grid container direction="column">
